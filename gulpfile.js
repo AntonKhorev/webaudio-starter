1fc3a94cd0f7f209c844154a80f4f46ae7172c55
Anton Khorev <tony29@yandex.ru>
2016-02-23 02:07:07 +0300

    updated highlight.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,6 @@ const gulpTasks=require('crnx-build/gulp-tasks');
 gulpTasks(
 	gulp,
 	"WebAudio demo",
-	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.7/styles/default.min.css'],
-	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.7/highlight.min.js']
+	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/styles/default.min.css'],
+	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/highlight.min.js']
 );
