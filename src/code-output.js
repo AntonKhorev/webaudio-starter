3f9180daad368f7459457fd7bd67de2ce98c3480
Anton Khorev <tony29@yandex.ru>
2016-04-15 17:22:36 +0300

    use new oop code-output interface

--- src/code-output.js
+++ src/code-output.js
@@ -3,17 +3,9 @@
 const BaseCodeOutput=require('crnx-base/code-output')
 
 class CodeOutput extends BaseCodeOutput {
-	constructor(generateCode,i18n) {
-		super(generateCode,i18n)
-		//const interestingWarningText=i18n('code-output.warning.interesting')
-		const interestingWarningText="You need at least one source and some processing enabled to get interesting JavaScript code"
-		this.$htmlInterestingWarning=$("<span class='tip-warn'><span class='tip-content'>"+interestingWarningText+"</span></span>")
-		this.$jsInterestingWarning=$("<span class='tip-warn'><span class='tip-content'>"+interestingWarningText+"</span></span>")
-		this.$sections['html'].find('summary').append(" ",this.$htmlInterestingWarning)
-		this.$sections['js'].find('summary').append(" ",this.$jsInterestingWarning)
-	}
-	get refs() {
-		return {
+	get defaultFormatting() {
+		const formatting=super.defaultFormatting
+		formatting.refs={
 			context:     'https://www.w3.org/TR/webaudio/#AudioContext',
 			source:      'https://www.w3.org/TR/webaudio/#MediaElementAudioSourceNode',
 			gain:        'https://www.w3.org/TR/webaudio/#GainNode',
@@ -26,16 +18,33 @@ class CodeOutput extends BaseCodeOutput {
 			compressor:  'https://www.w3.org/TR/webaudio/#the-dynamicscompressornode-interface',
 			analyser:    'https://www.w3.org/TR/webaudio/#the-analysernode-interface',
 		}
+		return formatting
 	}
-	writeButtons(getFormatting,i18n) {
-		return super.writeButtons(getFormatting,i18n).append(
+	initDomProps() {
+		super.initDomProps()
+		this.$warnings=$()
+	}
+	applyUpdatedCode(i18n) {
+		super.applyUpdatedCode(i18n)
+		this.$warnings.toggle(!this.code.isInteresting)
+	}
+	writeButtons(i18n) {
+		return super.writeButtons(i18n).append(
 			" <span class='tip-warn'><span class='tip-content'>"+i18n('code-output.warning.jsfiddle-run')+"</span></span>"
 		)
 	}
-	actualUpdate(generateCode,extractCode) {
-		super.actualUpdate(generateCode,extractCode)
-		this.$htmlInterestingWarning.toggle(!this.code.isInteresting)
-		this.$jsInterestingWarning.toggle(!this.code.isInteresting)
+	writeSectionSummary(sectionName,i18n) {
+		const $section=super.writeSectionSummary(sectionName,i18n)
+		const interestingWarningText="You need at least one source and some processing enabled to get interesting JavaScript code" // TODO i18n
+		if (sectionName=='html' || sectionName=='js') {
+			let $warning
+			$section.append(
+				" ",
+				$warning=$("<span class='tip-warn'><span class='tip-content'>"+interestingWarningText+"</span></span>")
+			)
+			this.$warnings=this.$warnings.add($warning)
+		}
+		return $section
 	}
 }
 
