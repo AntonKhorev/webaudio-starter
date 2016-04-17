287116af56248e7d58667e6068ebb7a1075cab58
Anton Khorev <tony29@yandex.ru>
2016-04-17 13:43:41 +0300

    coefs for all biquad filter types

--- src/biquad-filter-option-output.js
+++ src/biquad-filter-option-output.js
@@ -7,6 +7,7 @@ class BiquadFilterOptionOutput extends FilterOptionOutput {
 		super(option,writeOption,i18n,generateId)
 		let $cloneButton
 		const computeCoefs=(type,frequency,detune,Q,gain)=>{
+			// constants defined in the spec
 			//const F_s=audioContext.sampleRate // TODO pass audioContext
 			const F_s=48000
 			//
@@ -17,12 +18,69 @@ class BiquadFilterOptionOutput extends FilterOptionOutput {
 			const alpha_Q=Math.sin(omega_0)/(2*Q)
 			const Q_B=Math.pow(10,Q/20) // fix for incorrect alpha_B
 			const alpha_B=Math.sin(omega_0)/2*Math.sqrt((4-Math.sqrt(16-16/(Q_B*Q_B)))/2) // was incorrect in the spec
+			const S=1
+			const alpha_S=Math.sin(omega_0)/2*Math.sqrt((A+1/A)*(1/S-1)+2)
+
+			// shortcuts
 			const c=Math.cos(omega_0)
+			const aSA2=2*alpha_S*Math.sqrt(A)
+
 			switch (type) {
 			case 'lowpass':
 				return {
 					feedforward: [(1-c)/2,1-c,(1-c)/2],
 					feedback: [1+alpha_B,-2*c,1-alpha_B],
+				}
+			case 'highpass':
+				return {
+					feedforward: [(1+c)/2,-(1+c),(1+c)/2],
+					feedback: [1+alpha_B,-2*c,1-alpha_B],
+				}
+			case 'bandpass':
+				return {
+					feedforward: [alpha_Q,0,-alpha_Q],
+					feedback: [1+alpha_Q,-2*c,1-alpha_Q],
+				}
+			case 'notch':
+				return {
+					feedforward: [1,-2*c,1],
+					feedback: [1+alpha_Q,-2*c,1-alpha_Q],
+				}
+			case 'allpass':
+				return {
+					feedforward: [1-alpha_Q,-2*c,1+alpha_Q],
+					feedback: [1+alpha_Q,-2*c,1-alpha_Q],
+				}
+			case 'peaking':
+				return {
+					feedforward: [1+alpha_Q*A,-2*c,1-alpha_Q*A],
+					feedback: [1+alpha_Q/A,-2*c,1-alpha_Q/A],
+				}
+			case 'lowshelf':
+				return {
+					feedforward: [
+						   A*( (A+1) - (A-1)*c + aSA2 ),
+						 2*A*( (A-1) - (A+1)*c ),
+						   A*( (A+1) - (A-1)*c - aSA2 ),
+					],
+					feedback: [
+						       (A+1) + (A-1)*c + aSA2,
+						  -2*( (A-1) + (A+1)*c ),
+						       (A+1) + (A-1)*c - aSA2,
+					],
+				}
+			case 'highshelf':
+				return {
+					feedforward: [
+						   A*( (A+1) + (A-1)*c + aSA2 ),
+						-2*A*( (A-1) + (A+1)*c ),
+						   A*( (A+1) + (A-1)*c - aSA2 ),
+					],
+					feedback: [
+						       (A+1) - (A-1)*c + aSA2,
+						   2*( (A-1) - (A+1)*c ),
+						       (A+1) - (A-1)*c - aSA2,
+					],
 				}
 			}
 		}
