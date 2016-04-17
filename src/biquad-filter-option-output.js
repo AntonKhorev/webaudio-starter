dc07c1528d1a6ce3a04d969dd6ab6724acb318ae
Anton Khorev <tony29@yandex.ru>
2016-04-17 13:03:20 +0300

    rewrote lowpass filter coefs computation

--- src/biquad-filter-option-output.js
+++ src/biquad-filter-option-output.js
@@ -7,19 +7,23 @@ class BiquadFilterOptionOutput extends FilterOptionOutput {
 		super(option,writeOption,i18n,generateId)
 		let $cloneButton
 		const computeCoefs=(type,frequency,detune,Q,gain)=>{
-			// TODO frequency with detune
-			// LPF
-			const G=Math.pow(10,Q/20)
-			//const omega_0=2*Math.PI*frequency/audioContext.sampleRate // TODO pass audioContext
-			const omega_0=2*Math.PI*frequency/48000
+			//const F_s=audioContext.sampleRate // TODO pass audioContext
+			const F_s=48000
 			//
+			const f_0=frequency*Math.pow(2,detune/1200)
+			const G=gain
+			const A=Math.pow(10,G/40)
+			const omega_0=2*Math.PI*f_0/F_s
 			const alpha_Q=Math.sin(omega_0)/(2*Q)
-			const alpha_B=Math.sin(omega_0)/2*Math.sqrt((4-Math.sqrt(16-16/(G*G)))/2)
-			const alpha=alpha_B
+			const Q_B=Math.pow(10,Q/20) // fix for incorrect alpha_B
+			const alpha_B=Math.sin(omega_0)/2*Math.sqrt((4-Math.sqrt(16-16/(Q_B*Q_B)))/2) // was incorrect in the spec
 			const c=Math.cos(omega_0)
-			return {
-				feedforward: [(1-c)/2,1-c,(1-c)/2],
-				feedback: [1+alpha,-2*c,1-alpha],
+			switch (type) {
+			case 'lowpass':
+				return {
+					feedforward: [(1-c)/2,1-c,(1-c)/2],
+					feedback: [1+alpha_B,-2*c,1-alpha_B],
+				}
 			}
 		}
 		this.$output.append(
