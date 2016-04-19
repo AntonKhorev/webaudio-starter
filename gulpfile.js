c8c3294d9e3593a83d0ad91ce7a63dc1510f393a
Anton Khorev <tony29@yandex.ru>
2016-04-19 14:21:22 +0300

    updated highlight.js - fixes unquoted html attrs highlighting

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulpTasks(
 		en: "Web Audio API example generator",
 		ru: "Генератор примеров Web Audio API",
 	},
-	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/styles/default.min.css'],
-	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/highlight.min.js'],
+	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/default.min.css'],
+	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js'],
 	[require.resolve('crnx-base')]
 )
