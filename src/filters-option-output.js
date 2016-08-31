111dd06ba333d60e1a80781f591680a6755eccf5
Anton Khorev <tony29@yandex.ru>
2016-08-31 12:44:18 +0300

    graph editor: move nodes

--- src/filters-option-output.js
+++ src/filters-option-output.js
@@ -8,20 +8,43 @@ class FiltersOptionOutput extends ArrayOptionOutput {
 		super(option,writeOption,i18n,generateId)
 		const gridSize=32
 		const gridHeight=15
+		let $nodes
+		let $movedNode=null
+		let movedX,movedY
+		const writeNode=(nodeName,x,y)=>$("<div class='node'>").append(
+			$("<div class='node-section node-section-head'>").html(
+				i18n(nodeName)
+			),
+			$("<div class='node-section'>")
+		).css({
+			left: x,
+			top: y,
+		}).mousedown(function(ev){
+			const $node=$(this)
+			$nodes.append($node)
+			$movedNode=$node
+			movedX=ev.pageX
+			movedY=ev.pageY
+		})
 		this.$output.prepend(
 			$("<div class='graph'>").height(gridSize*gridHeight).append(
-				$("<div class='node'>").append(
-					$("<div class='node-section node-section-head'>").html(
-						i18n('options.sources')
-					),
-					$("<div class='node-section'>")
-				),
-				$("<div class='node'>").append(
-					$("<div class='node-section node-section-head'>").html(
-						i18n('options.destination')
-					),
-					$("<div class='node-section'>")
-				)
+				$nodes=$("<div class='nodes'>").append(
+					writeNode('options.sources',2*gridSize,5*gridSize),
+					writeNode('options.destination',12*gridSize,6*gridSize)
+				).mousemove(function(ev){
+					if (!$movedNode) return
+					const dx=ev.pageX-movedX
+					const dy=ev.pageY-movedY
+					const pos=$movedNode.position()
+					$movedNode.css({
+						left: pos.left+dx,
+						top: pos.top+dy,
+					})
+					movedX+=dx
+					movedY+=dy
+				}).mouseup(function(){
+					$movedNode=null
+				})
 			)
 		)
 	}
