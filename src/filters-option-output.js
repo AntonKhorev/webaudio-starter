d5f573a22067897a202cb863159103c70ea1c52e
Anton Khorev <tony29@yandex.ru>
2016-08-31 13:53:53 +0300

    graph editor: svg layer

--- src/filters-option-output.js
+++ src/filters-option-output.js
@@ -8,17 +8,21 @@ class FiltersOptionOutput extends ArrayOptionOutput {
 		super(option,writeOption,i18n,generateId)
 		const gridSize=32
 		const gridHeight=15
-		let $nodes
+		let $lines,$nodes
 		let $movedNode=null
 		let movedX,movedY
+		//const animations=new Map // can jQuery animations do it?
+		const writeLine=(x1,y1,x2,y2)=>$(
+			document.createElementNS("http://www.w3.org/2000/svg","line")
+		).attr({stroke:'#000',x1,y1,x2,y2})
 		const writeNode=(nodeName,x,y)=>$("<div class='node'>").append(
 			$("<div class='node-section node-section-head'>").html(
 				i18n(nodeName)
 			),
 			$("<div class='node-section'>")
 		).css({
-			left: x,
-			top: y,
+			left: x*gridSize,
+			top: y*gridSize,
 		}).mousedown(function(ev){
 			const $node=$(this)
 			$nodes.append($node)
@@ -28,9 +32,12 @@ class FiltersOptionOutput extends ArrayOptionOutput {
 		})
 		this.$output.prepend(
 			$("<div class='graph'>").height(gridSize*gridHeight).append(
+				$lines=$("<svg xmlns='http://www.w3.org/2000/svg' version='1.1'></svg>").append(
+					writeLine(100,100,400,200)
+				),
 				$nodes=$("<div class='nodes'>").append(
-					writeNode('options.sources',2*gridSize,5*gridSize),
-					writeNode('options.destination',12*gridSize,6*gridSize)
+					writeNode('options.sources',2,5),
+					writeNode('options.destination',12,6)
 				).mousemove(function(ev){
 					if (!$movedNode) return
 					const dx=ev.pageX-movedX
