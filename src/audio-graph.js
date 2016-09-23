9a118dcada616563072218447ff442d655e25d5e
Anton Khorev <tony29@yandex.ru>
2016-09-23 23:31:32 +0300

    graph editor: remove passive nodes

--- src/audio-graph.js
+++ src/audio-graph.js
@@ -8,31 +8,58 @@ const Feature=require('./feature')
 class AudioGraph extends Feature {
 	constructor(options) {
 		super()
-		const createNodes=()=>{ // returns "indexed" nodes = nodes in order of the indexes used by options to represent graph edges
+		const createNodes=()=>{
+			const distinctNodes=new Set()
 			const destinationNode=new Node.destination
-			return options.nodes.map(nodeOptions=>{
+			const indexedNodes=options.nodes.map(nodeOptions=>{
 				let node=new Node[nodeOptions.nodeType](nodeOptions)
 				if (node instanceof Node.destination) {
 					node=destinationNode
 				}
+				distinctNodes.add(node)
 				return node
 			})
-		}
-		const connectNodes=(indexedNodes)=>{
 			for (let i=0;i<options.nodes.length;i++) {
 				for (const j of options.nodes[i].next) {
 					indexedNodes[i].nextNodes.add(indexedNodes[j])
 					indexedNodes[j].prevNodes.add(indexedNodes[i])
 				}
 			}
+			const outputNodes=[]
+			distinctNodes.forEach(node=>{
+				outputNodes.push(node)
+			})
+			return outputNodes
 		}
 		// TODO propagate upstream/downstream effects, clean up nodes that don't have both
 		// 	can try to combine it with assignNumbersToNodes
 		//		const liveNodes=new Set
 		//		const liveNodesRec=(node)=>{
 		//	then will have to assign numbers after cleaning up
-		const sortNodes=(indexedNodes)=>{
-			const sortedNodes=[]
+		const removePassiveNodes=(inputNodes)=>{
+			const outputNodes=[]
+			for (const node of inputNodes) {
+				if (!node.passive) {
+					outputNodes.push(node)
+				} else {
+					node.prevNodes.forEach(prevNode=>{
+						prevNode.nextNodes.delete(node)
+						node.nextNodes.forEach(nextNode=>{
+							prevNode.nextNodes.add(nextNode)
+						})
+					})
+					node.nextNodes.forEach(nextNode=>{
+						nextNode.prevNodes.delete(node)
+						node.prevNodes.forEach(prevNode=>{
+							nextNode.prevNodes.add(prevNode)
+						})
+					})
+				}
+			}
+			return outputNodes
+		}
+		const sortNodes=(inputNodes)=>{
+			const outputNodes=[]
 			const visited=new Set
 			const classCounts=new Map
 			const classLastNodes=new Map
@@ -45,19 +72,19 @@ class AudioGraph extends Feature {
 				classCounts.set(proto,n)
 				classLastNodes.set(proto,node)
 				node.n=n
-				sortedNodes.push(node)
+				outputNodes.push(node)
 			}
-			indexedNodes.forEach(rec)
+			inputNodes.forEach(rec)
 			classCounts.forEach((n,proto)=>{
 				if (n==1) {
 					delete classLastNodes.get(proto).n
 				}
 			})
-			return sortedNodes
+			return outputNodes
 		}
-		const indexedNodes=createNodes()
-		connectNodes(indexedNodes)
-		this.nodes=sortNodes(indexedNodes)
+		const createdNodes=createNodes()
+		const filteredNodes=removePassiveNodes(createdNodes)
+		this.nodes=sortNodes(filteredNodes)
 	}
 	requestFeatureContext(featureContext) {
 		for (const node of this.nodes) {
