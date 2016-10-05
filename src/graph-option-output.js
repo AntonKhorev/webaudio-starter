4b9b6091cec16e396e2f4476b6fb8a1f2c608e5a
Anton Khorev <tony29@yandex.ru>
2016-10-05 15:05:21 +0300

    graph editor: have to use wrapper div for node fieldsets to accomodate chrome

--- src/graph-option-output.js
+++ src/graph-option-output.js
@@ -8,9 +8,11 @@ const Option=require('./option-classes')
 class NodeOptionsOutput extends BaseOptionsOutput {
 	setOptionClassWriters(optionClassWriters) {
 		optionClassWriters.set(Option.Group,(option,writeOption,i18n,generateId)=>{
-			return option.$=$("<fieldset class='node-option'>").append(
-				"<legend>"+i18n('options.'+option.fullName)+"</legend>",
-				option.entries.map(writeOption)
+			return option.$=$("<div class='node-fieldset-container'>").append(
+				$("<fieldset class='node-option'>").append(
+					"<legend>"+i18n('options.'+option.fullName)+"</legend>",
+					option.entries.map(writeOption)
+				)
 			)
 		})
 		optionClassWriters.set(Option.Text,(option,writeOption,i18n,generateId)=>{ // TODO fix mostly copypaste from crnx-base
@@ -297,7 +299,7 @@ class GraphOptionOutput {
 		}
 		const addNode=(nodeOption,gx0,gy0)=>{
 			const nodeOptionsOutput=new NodeOptionsOutput({root: nodeOption},generateId,i18n)
-			const $node=nodeOptionsOutput.$output
+			const $node=nodeOptionsOutput.$output.children('fieldset')
 			const id=generateId()
 			let dragAnimationId=null
 			let snapAnimationId=null
